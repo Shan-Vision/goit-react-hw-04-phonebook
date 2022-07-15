6fcf131d56b6f00dd0ffb5ebed0bd2c974034a30
Sherzod Abdullaev <perfekt.vision2019@gmail.com>
2022-07-15 18:32:37 +0200

    minor refactoring from class into hooks in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,145 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from 'components/Form';
 import ContactList from 'components/ContactList';
 import FilterContacts from 'components/FilterContacts';
 import shortid from 'shortid';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+function SaveIntoLocalStorage() {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('state')) ?? [];
+  });
 
-  componentDidMount = () => {
-    const contactsFromLocalStorage = localStorage.getItem('contacts');
-    const parsedStorageContacts = JSON.parse(contactsFromLocalStorage);
-    if (parsedStorageContacts) {
-      this.setState({ contacts: parsedStorageContacts });
-    }
-  };
-
-  componentDidUpdate = (prevProps, prevState) => {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  };
-
-  handleSubmitForm = ({ contact }) => {
-    const { name, number } = contact;
-
-    this.setState(state => {
-      return {
-        contacts: [...state.contacts, { id: shortid.generate(), name, number }],
-      };
-    });
-  };
-
-  filterContactsByName = () =>
-    this.state.contacts.filter(elem =>
-      elem.name.toLowerCase().includes(this.state.filter)
-    );
-
-  handleFilterChange = event => {
-    this.setState({ filter: event.target.value.toLowerCase() });
-  };
-
-  deleteContactById = contactId => {
-    this.setState(state => {
-      return {
-        contacts: state.contacts.filter(elem => elem.id !== contactId),
-      };
-    });
-  };
-  render() {
-    const contactNameList = this.state.contacts.map(contact => contact.name);
-
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 20,
-          color: '#010101',
-        }}
-      >
-        <h1>Phonebook</h1>
-        <ContactForm
-          onSubmit={this.handleSubmitForm}
-          contactNameList={contactNameList}
-        />
-        <h2>Contacts</h2>
-        <FilterContacts onChange={this.handleFilterChange} />
-        <ContactList
-          contacts={this.filterContactsByName()}
-          onDeleteId={this.deleteContactById}
-        />
-      </div>
-    );
-  }
+  useEffect(() => {
+    window.localStorage.setItem('state', JSON.stringify(state));
+  }, [state]);
+  return [state, setState];
 }
 
+const App = () => {
+  const [contacts, setContacts] = SaveIntoLocalStorage();
+  const [filter, setFilter] = useState('');
+
+  const handleSubmitForm = ({ contact }) => {
+    const { name, number } = contact;
+
+    setContacts([...contacts, { id: shortid.generate(), name, number }]);
+  };
+
+  const filterContactsByName = () =>
+    contacts.filter(elem => elem.name.toLowerCase().includes(filter));
+
+  const handleFilterChange = event => {
+    setFilter(event.target.value.toLowerCase());
+  };
+
+  const deleteContactById = contactId => {
+    setContacts(contacts.filter(elem => elem.id !== contactId));
+  };
+
+  const contactNameList = contacts.map(contact => contact.name);
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 20,
+        color: '#010101',
+      }}
+    >
+      <h1>Phonebook</h1>
+      <ContactForm
+        onSubmit={handleSubmitForm}
+        contactNameList={contactNameList}
+      />
+      <h2>Contacts</h2>
+      <FilterContacts onChange={handleFilterChange} />
+      <ContactList
+        contacts={filterContactsByName()}
+        onDeleteId={deleteContactById}
+      />
+    </div>
+  );
+};
+
 export default App;
+
+// class OldApp extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//   componentDidMount = () => {
+//     const contactsFromLocalStorage = localStorage.getItem('contacts');
+//     const parsedStorageContacts = JSON.parse(contactsFromLocalStorage);
+//     if (parsedStorageContacts) {
+//       this.setState({ contacts: parsedStorageContacts });
+//     }
+//   };
+
+//   componentDidUpdate = (prevProps, prevState) => {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   };
+
+//   handleSubmitForm = ({ contact }) => {
+//     const { name, number } = contact;
+
+//     this.setState(state => {
+//       return {
+//         contacts: [...state.contacts, { id: shortid.generate(), name, number }],
+//       };
+//     });
+//   };
+
+//   filterContactsByName = () =>
+//     this.state.contacts.filter(elem =>
+//       elem.name.toLowerCase().includes(this.state.filter)
+//     );
+
+//   handleFilterChange = event => {
+//     this.setState({ filter: event.target.value.toLowerCase() });
+//   };
+
+//   deleteContactById = contactId => {
+//     this.setState(state => {
+//       return {
+//         contacts: state.contacts.filter(elem => elem.id !== contactId),
+//       };
+//     });
+//   };
+//   render() {
+//     const contactNameList = this.state.contacts.map(contact => contact.name);
+
+//     return (
+//       <div
+//         style={{
+//           height: '100vh',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           justifyContent: 'center',
+//           alignItems: 'center',
+//           fontSize: 20,
+//           color: '#010101',
+//         }}
+//       >
+//         <h1>Phonebook</h1>
+//         <ContactForm
+//           onSubmit={this.handleSubmitForm}
+//           contactNameList={contactNameList}
+//         />
+//         <h2>Contacts</h2>
+//         <FilterContacts onChange={this.handleFilterChange} />
+//         <ContactList
+//           contacts={this.filterContactsByName()}
+//           onDeleteId={this.deleteContactById}
+//         />
+//       </div>
+//     );
+//   }
+// }
